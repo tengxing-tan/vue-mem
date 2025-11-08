6663956f1bc2fb513bb2bd61a47c70d49598fe36
Teng Xing Tan <TANT441323@exactsoftware.com>
2025-11-08 18:11:37 +0800

    build error

--- src/views/member/MemberRead.vue
+++ src/views/member/MemberRead.vue
@@ -10,9 +10,9 @@ const props = defineProps<{
 const { setPhoneNoSearchKey, member } = useMemberGetStore()
 setPhoneNoSearchKey(props.phoneNo)
 
-function formatMemberDate(value: unknown): string {
+function formatMemberDate(value: string | Date): string {
   if (!value) return ''
-  const d = new Date(value as any)
+  const d = new Date(value)
   if (isNaN(d.getTime())) return ''
   // return d.toUTCString()
   return d.toDateString()
